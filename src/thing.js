97d2416f822aeb6b2e441f3e0976bac6d061d3c5
Alan <alin@2u.com>
2015-04-19 17:00:49 -0400

    feature(thing): add basic inheritance lib'

--- src/thing.js
+++ src/thing.js
@@ -1,8 +1,70 @@
+// Is the convention to call it global?
 (function (global) {
 
   function Thing() {
     return "Sup";
   }
-  
+
+  // What we will start with is by creating a proper constructor 
+  Thing.create = function (options) {
+    var Constructor = options.init;
+    var prototype = Constructor.prototype;
+
+    if (options.extend) {
+      if (typeof options.extend !== "function") {
+        throw "options.extend must be a function.";
+      }
+      Constructor.prototype = extend();
+    }
+
+    getMethods(options);
+
+    return Constructor;
+
+    function getMethods () {
+      var reserved = [
+        'init',
+        'extend'
+      ];
+      for (var i in options) {
+        if (options.hasOwnProperty(i) && reserved.indexOf(i) < 0) {
+          Constructor.prototype[i] = options[i];
+        }
+      }
+    };
+
+    function extend () {
+      var parentConstructor = options.extend;
+       if (typeof global.Object.create === "function") {
+         return global.Object.create(parentConstructor.prototype);
+       } else {
+         var fn = function () {};
+         fn.prototype = parentConstructor.prototype;
+         return new fn();
+       }
+    };
+
+  };
+
+
+
+  /*
+   * var Person = Thing.create({
+   *    init: function () {
+   *    },
+   *    eat: function () {
+   *    },
+   *    fly: function () {
+   *    },
+   *    extends: Being
+   *  });
+   *
+   *  var Person = function () {
+   *      this.haha = "wow";
+   *  };
+   */
+
+ 
+  // What happens when there is already something called thing? 
   global.Thing = Thing;
 })(window);
--- test/thing-spec.js
+++ test/thing-spec.js
@@ -2,4 +2,23 @@ describe('Thing', function () {
   it('says hello world', function () {
     expect(Thing()).toEqual("Sup");
   });
+  it('random', function () {
+    var Msg = Thing.create({
+      init: function () {},
+      sup: function () { console.log("dawg"); }
+    });
+    var Wow = Thing.create({
+      init: function () {
+        console.log("constructor");
+      },
+      greet: function () {
+        console.log("This is a greeting");
+      },
+      extend: Msg
+    });
+
+    var n = new Wow();
+    n.greet();
+    n.sup();
+  });
 });
